d91000c534c5f0de4654355fb18581253e2b8188
Leszek Kwaśniewski <leszek@lk-net.pl>
2025-06-29 12:40:36 +0200

    returned to predicitons and added check on git_hash

--- ARISA_DSML/predict.py
+++ ARISA_DSML/predict.py
@@ -122,8 +122,9 @@ if __name__ == "__main__":
     analysis_df["prediction"] = df_preds["prediction"]
     analysis_df["predicted_probability"] = df_preds["predicted_probability"]
     git_hash = get_git_commit_hash()
-    mlflow.set_experiment("diabetes_hyperparam_tuning_v2")
-    with mlflow.start_run(tags={"git_sha": git_hash}):
+    mlflow.set_experiment("diabetes_predictions")
+    logger.info(f"Starting MLflow run with git_sha: {git_hash}")
+    with mlflow.start_run(tags={"git_sha": str(git_hash or "unknown")}):
         estimated_performance = estimator.estimate(analysis_df)
         mlflow.log_figure(estimated_performance.plot(), "estimated_performance.png")
 
